822e75156606f8d1d2601ed5af3ba7b0390a4fd8
SBarn11 <sarabarnett1@outlook.com>
2022-04-10 04:33:32 -0600

    initial file setup and create models

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Social-Network-mongoapp
+# NoSQLSocial-Network-mongoapp
+
+
+## Description
+This project is a command-line application built to manage a company's employee database, using Node.js, Inquirer, and MySQL.
+
+## Usage
+Please refer to the following link to view a video demo to see how the app can be used.
+
+
+## Screenshot
+Below is a screenshot of the command-line prompts as well as the generated HTML rendered in the browser.
+
+![command line screenshot](./assets/images/screenshot1.jpg)
+
+
+![command line screenshot](./assets/images/screenshot2.jpg)
+
+
+## Contributors
+This challenge was coded by Sara Barnett.
---
+++ models/Thought.js
@@ -0,0 +1,64 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId()
+  },
+  reactionBody: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: 'Username is required'
+  },
+  createdAt: {
+    type: Date, 
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal)
+  }
+},
+ {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+ });
+
+
+const ThoughtSchema = new Schema ({
+  thoughtText: {
+    type: String, 
+    required: 'Thought is required',
+    validate: [({ length }) => length < 280, 'Thought must be less than 280 characters.']
+  },
+  username: {
+    type: String,
+    required: 'Username required.'
+  },
+  createAt: {
+    type: Date, 
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal)
+  },
+  reactions: [ReactionSchema],
+},
+{
+  toJSON: {
+    virtuals: true,
+    getters: true
+  },
+  id: false
+},
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
